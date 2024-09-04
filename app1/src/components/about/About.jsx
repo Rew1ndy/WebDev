afdc2f208550faed482909dbab4a90acb767ff36
Rew1nd ⠀ <bogdin200056@gmail.com>
2024-09-04 14:00:04 +0300

    Lab1

---
+++ app1/src/components/about/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import "./about.css";
+
+const About = () => {
+    return (<>
+        <section className="About">
+            About Page ;)
+        </section>
+    </>)
+}
+
+export {About}
